d9268903dd3a011ec56855b77b7581f185373642
yusufsargin <yusufsargin9@gmail.com>
2020-09-03 11:06:13 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './application.module';
 async function bootstrap() {
   const server = await NestFactory.create(AppModule);
 
-  await server.listen(3000);
+  await server.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
